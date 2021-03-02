d52026d8544ca1b33016f9da2c3d4d59b2eabe5e
Sucs-God <technogen228@gmail.com>
2021-03-02 20:50:55 +0300

    [code] Js Lessons Object 11

--- js/app.js
+++ js/app.js
@@ -144,4 +144,35 @@
 // console.log(cars)
 
 // 10 Циклы
-const cars = ['Мазда', 'Мерседес', 'Форд']
+// const  cars = ['Мазда', 'Мерседес', 'Форд', 'Porsche']
+
+// for (let i = 0; i < cars.length; i++) {
+//     const car = cars[i]
+//     console.log(car)
+// }
+
+// for (let car of cars) {
+//     console.log(car)
+// }
+
+// 11 Объекты
+const person = {
+    firstName: 'Vladilen',
+    lastName: 'Minin',
+    year: 1993,
+    languages: ['Ru', 'Eu', 'De'],
+    hasWife: false,
+    greet: function () {
+        console.log('greet from person')
+    }
+}
+
+console.log(person.firstName)
+console.log(person['lastName'])
+const key = 'year'
+console.log(person[key])
+person.hasWife = true
+person.isProgrammer = true
+console.log(person)
+
+person.greet()
